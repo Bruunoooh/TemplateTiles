c1dad0880a1f2ec480901a030475b2a8e5fc5e06
Bruunoooh <bruno.sharp@live.com>
2013-05-03 22:22:22 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+<link href="style.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+
+<div id="header">
+<div id="logo"><img src="http://i.imgur.com/6kdwN7w.png" /></div>
+</div><br /><br /><br /><br /><br />
+
+<div id="principalleft">
+<div id="redesocial">
+<span style="color: #999">Rede Sociais</span><br /><hr width="80%" color="#f3f3f3" />
+<a href="#"><li id="fb">f</li></a>
+<a href="#"><li id="tw">t</li></a>
+<a href="#"><li id="gm">g<font size="4">+</font></li></a>
+</div>
+</div>
+
+<div align="center">
+  <script type="text/javascript" src="script.js"></script>
+</div>
+</body>
+</html>
